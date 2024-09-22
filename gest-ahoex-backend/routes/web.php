2320a644626c8f1033fe21543848fa943b8e0a34
Arieleandrovv <Arieleandrovv@outlook.com>
2024-09-22 19:46:09 -0400

    creacion de rutas

--- gest-ahoex-backend/routes/web.php
+++ gest-ahoex-backend/routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarreraController;
+
+Route::controller(CarreraController::class)->group(function (){
+    Route::get('/carreras', 'index');
+    //Route::get('carreras/{id}', 'show');
+    //Route::post('carreras', 'store');
+    //Route::put('carreras/{id}', 'update');
+    //Route::delete('carreras/{id}', 'destroy');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
