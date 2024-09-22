b5ddb1138cef4391442d1a97e80174782f21970f
Arieleandrovv <Arieleandrovv@outlook.com>
2024-09-22 19:36:02 -0400

    controlador de carreras

--- gest-ahoex-backend/app/Http/Controllers/CarreraController.php
+++ gest-ahoex-backend/app/Http/Controllers/CarreraController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Carrera;
 class CarreraController extends Controller
 {
     /**
@@ -11,7 +11,8 @@ class CarreraController extends Controller
      */
     public function index()
     {
-        //
+        $carreras = Carrera::all();
+        return $carreras;
     }
 
     /**
