adff61035ebb932473c24dd2665fa4c0a6f294b1
Jairo Andree Maida Rojas <jairotrabaja123@gmail.com>
2024-09-23 11:31:39 -0400

    fix: moving routes from web to api

--- gest-ahoex-backend/routes/web.php
+++ gest-ahoex-backend/routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CarreraController;
 
-Route::controller(CarreraController::class)->group(function (){
-    Route::get('carreras', 'index');
-    //Route::get('carreras/{id}', 'show');
-    //Route::post('carreras', 'store');
-    //Route::put('carreras/{id}', 'update');
-    //Route::delete('carreras/{id}', 'destroy');
-});
+
 
 /*
 Route::get('/', function () {
