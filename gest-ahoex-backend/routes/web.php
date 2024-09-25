6ac2a5838e56446d7eb6b1a9aaabf679fcb0d637
Samres27 <yosoyam4@gmail.com>
2024-09-25 09:44:45 -0400

    fix: se añadio la ruta

--- gest-ahoex-backend/routes/web.php
+++ gest-ahoex-backend/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controller\CurriculaController;
 
 
 /*
 Route::get('/', function () {
     return view('welcome');
 });*/
+
+Route::post('/curricula/update',[CurriculaController::class, 'update']);
