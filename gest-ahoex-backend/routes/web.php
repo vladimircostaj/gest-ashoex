1f0b9e98752192102e735fa3f18d1d97681b1c02
Samres27 <yosoyam4@gmail.com>
2024-09-25 09:47:30 -0400

    fix: se añadio la ruta para el eliminar

--- gest-ahoex-backend/routes/web.php
+++ gest-ahoex-backend/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controller\CurriculaController;
 
 
 /*
 Route::get('/', function () {
     return view('welcome');
 });*/
+
+Route::post('/curricula/eliminar',[CurriculaController::class, 'destroy']);
