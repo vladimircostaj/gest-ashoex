d0c4138a367f4b9dd5b65590ff451eff236d15e3
NicolasVia <128348720+NicolasVia@users.noreply.github.com>
2024-09-29 18:28:57 -0400

    backend: Creacion de endpoint para update

---
+++ gest-ashoex-backend/app/Http/Controllers/PersonalAcademicoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PersonalAcademico;
+use Illuminate\Http\Request;
+
+class PersonalAcademicoController extends Controller
+{
+
+    public function update(Request $request, $id)
+    {
+
+        $personalAcademico = PersonalAcademico::find($id);
+
+        if (!$personalAcademico) {
+            return response()->json([
+                'message' => 'Personal académico no encontrado.'
+            ], 404);
+        }
+
+        $personalAcademico->name = $request->input('name');
+        $personalAcademico->email = $request->input('email');
+        $personalAcademico->telefono = $request->input('telefono');
+        $personalAcademico->estado = $request->input('estado');
+        $personalAcademico->tipo_personal_id = $request->input('tipo_personal_id');
+
+        $personalAcademico->save();
+
+        return response()->json([
+            'message' => 'Personal académico actualizado exitosamente.',
+            'personal_academico' => $personalAcademico
+        ], 200);
+    }
+
+}
--- gest-ashoex-backend/routes/api.php
+++ gest-ashoex-backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PersonalAcademicoController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HealthController;
 
 Route::get('/health', [HealthController::class, 'check']);
+
+Route::put('/personal-academico/{id}', [PersonalAcademicoController::class,'update']);
