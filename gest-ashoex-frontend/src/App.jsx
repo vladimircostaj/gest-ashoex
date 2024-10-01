e450af3b0611ea915e115a2eb455366ddde55c82
Luis Fernando Cárdenas <201803077@est.umss.edu>
2024-09-30 20:15:45 -0400

    se implemento el fronted y back de lista de personal

--- gest-ashoex-frontend/src/App.jsx
+++ gest-ashoex-frontend/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import HealthCheck from './components/health/health.jsx'
 import './App.css'
-import ListaPersonalAcademico  from './components/ListaPersonalAcademico/ListaPersonalAcademico.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './pages/personal/InformacionPersonalAcademicoPage.jsx'
+import InformacionPersonalAcademico from './pages/personal/InformacionPersonalAcademicoPage';
+import RegistrarPersonalPage from './pages/registrar_personal_page.jsx';
+import ListaPersonalAcademico from './components/ListaPersonalAcademico/ListaPersonalAcademico.jsx';
 function App() {
+
 
   return (
     <>
-    <ListaPersonalAcademico/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HealthCheck/>} />
+        <Route path='/personal/:id/informacion' element={<InformacionPersonalAcademico/>} />
+        <Route path='/registrar-personal' element={<RegistrarPersonalPage/>} />
+        <Route path='/ListaPersonalAcademico' element={<ListaPersonalAcademico/>} />
+
+
+      </Routes>
+    </Router>
+   
     </>
   )
 }
+
 
 export default App
--- gest-ashoex-frontend/src/components/ListaPersonalAcademico/ListaPersonalAcademico.jsx
+++ gest-ashoex-frontend/src/components/ListaPersonalAcademico/ListaPersonalAcademico.jsx
@@ -3,7 +3,6 @@ import './ListaPersonalAcademico.css';
 import  ApiListaPersonalAcademico from  '../../services/ListaPersonalAcademico.js'
 function ListaPersonalAcademico() {
   const [listaPersonalAcademico, setListaPersonalAcademico] = useState([]);
-
   useEffect(() => {
     const obtenerDatos = async () => {
       try {
@@ -14,18 +13,14 @@ function ListaPersonalAcademico() {
         console.error(error);
       }
     };
-
     obtenerDatos();
   }, []);
-
-  const handleEdit = (personal) => {
+  const Editar = (personal) => {
   };
-
-  const handleDelete = (personalId) => {
+  const Borrar = (personalId) => {
   };
-
   return (
-    <div className="containerDoss" style={{ minHeight: '78.7vh' }}>
+    <div className="containerDoss" style={{ minHeight: '100vh' }}>
       <div className='encabezados'>
         <div className='contenidoss'>
           <h2 className='titulolistas'>Personal Académico Registrado:</h2>
@@ -48,14 +43,14 @@ function ListaPersonalAcademico() {
             {listaPersonalAcademico.map((personal) => (
               <tr key={personal.personal_academico_id} className="fila-lista">
                 <td>{personal.personal_academico_id}</td>
-                <td>{personal.nombre}</td>
+                <td>{personal.name}</td>
                 <td>{personal.email}</td>
                 <td>{personal.telefono}</td>
                 <td>{personal.Tipo_personal}</td>
                 <td>{personal.estado}</td>
                 <td>
-                  <button onClick={() => handleEdit(personal)} className="btn btn-primary">Editar</button>
-                  <button onClick={() => handleDelete(personal.personal_academico_id)} className="btn btn-danger">Eliminar</button>
+                  <button onClick={() => Editar(personal)} className="btn btn-primary">Editar</button>
+                  <button onClick={() => Borrar(personal.personal_academico_id)} className="btn btn-danger">Eliminar</button>
                 </td>
               </tr>
             ))}
@@ -65,74 +60,5 @@ function ListaPersonalAcademico() {
     </div>
   );
 }
-
 export default ListaPersonalAcademico;
-import React, { useState, useEffect } from 'react';
-import './ListaPersonalAcademico.css';
-import  ApiListaPersonalAcademico from  '../../services/ListaPersonalAcademico.js'
-function ListaPersonalAcademico() {
-  const [listaPersonalAcademico, setListaPersonalAcademico] = useState([]);
 
-  useEffect(() => {
-    const obtenerDatos = async () => {
-      try {
-        const data = await ApiListaPersonalAcademico();
-        console.log("datos", data);
-        setListaPersonalAcademico(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    obtenerDatos();
-  }, []);
-
-  const handleEdit = (personal) => {
-  };
-
-  const handleDelete = (personalId) => {
-  };
-
-  return (
-    <div className="containerDoss" style={{ minHeight: '78.7vh' }}>
-      <div className='encabezados'>
-        <div className='contenidoss'>
-          <h2 className='titulolistas'>Personal Académico Registrado:</h2>
-        </div>
-      </div>
-      <div className='tabla-contenedor'>
-        <table className="table table-hover">
-          <thead className="thead">
-            <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Email</th>
-              <th>Teléfono</th>
-              <th>Tipo Personal</th>
-              <th>Estado</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listaPersonalAcademico.map((personal) => (
-              <tr key={personal.personal_academico_id} className="fila-lista">
-                <td>{personal.personal_academico_id}</td>
-                <td>{personal.nombre}</td>
-                <td>{personal.email}</td>
-                <td>{personal.telefono}</td>
-                <td>{personal.Tipo_personal}</td>
-                <td>{personal.estado}</td>
-                <td>
-                  <button onClick={() => handleEdit(personal)} className="btn btn-primary">Editar</button>
-                  <button onClick={() => handleDelete(personal.personal_academico_id)} className="btn btn-danger">Eliminar</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-}
-
-export default ListaPersonalAcademico;
