03fd58baa8fbf7803dd59a703c16f95c670a0af2
jonathanalexiiz <jonathanasda@gmail.com>
2024-10-03 21:21:04 -0400

    Arreglado el cargo de dato de CarrerasSeeder, ahora carga los datos. Modifique en DatabaseSeeder.php

--- gest-ashoex-backend/database/seeders/DatabaseSeeder.php
+++ gest-ashoex-backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CarrerasSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +20,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Añadir la semilla CarrerasSeeders
+        $this->call(CarrerasSeeder::class);
     }
 }
