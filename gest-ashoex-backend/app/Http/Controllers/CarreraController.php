499d6158525bcf8c81221be50e1c2842b518bcb4
Arieleandrovv <Arieleandrovv@outlook.com>
2024-10-07 17:51:13 -0400

    modificacion de Carreras de acuerdo al formato establecido

--- gest-ashoex-backend/app/Http/Controllers/CarreraController.php
+++ gest-ashoex-backend/app/Http/Controllers/CarreraController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Carrera;
 use App\Http\Requests\CrearCarreraRequest;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Http\Response;
 
 class CarreraController extends Controller
 {
@@ -14,11 +15,47 @@ class CarreraController extends Controller
      * Display a listing of the resource.
      */
 
-    public function index()
-    {
-        $carreras = Carrera::all();
-        return response()->json($carreras, 200);
-    }
+     public function index()
+     {
+         try {
+             $carreras = Carrera::all();    
+                  if ($carreras->isEmpty()) {
+                 return response()->json([
+                     'success' => false,
+                     'data' => [],
+                     'error' => ['No se encontraron carreras'],
+                     'message' => 'Operación fallida'
+                 ], Response::HTTP_NOT_FOUND); 
+             }     
+             return response()->json([
+                 'success' => true,
+                 'data' => $carreras,
+                 'error' => [],
+                 'message' => 'Operación exitosa'
+             ], Response::HTTP_OK);     
+         } catch (\Illuminate\Database\QueryException $e) {
+             return response()->json([
+                 'success' => false,
+                 'data' => [],
+                 'error' => ['Error en la consulta a la base de datos'],
+                 'message' => 'Operación fallida'
+             ], Response::HTTP_BAD_REQUEST);     
+         } catch (\Illuminate\Auth\Access\AuthorizationException $e) {
+             return response()->json([
+                 'success' => false,
+                 'data' => [],
+                 'error' => ['No tienes permiso para acceder a estas carreras'],
+                 'message' => 'Operación fallida'
+             ], Response::HTTP_FORBIDDEN);     
+         } catch (\Exception $e) {
+             return response()->json([
+                 'success' => false,
+                 'data' => [],
+                 'error' => ['Ocurrió un error inesperado'],
+                 'message' => 'Operación fallida'
+             ], Response::HTTP_INTERNAL_SERVER_ERROR);
+         }
+     }
 
     /**
      * Store a newly created resource in storage.
