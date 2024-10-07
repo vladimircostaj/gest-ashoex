5f703cb780578b237500c320a6fa741f2e0b27ab
Arieleandrovv <Arieleandrovv@outlook.com>
2024-10-07 14:42:48 -0400

    correccion de database para funcion db:seeder

--- gest-ashoex-backend/database/seeders/DatabaseSeeder.php
+++ gest-ashoex-backend/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,10 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\CarrerasSeeder;
+use Database\Seeders\GruposSeeder;
+use Database\Seeders\MateriasSeeder;
+use Database\Seeders\CurriculaSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,15 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        // Añadir la semilla CarrerasSeeders
         $this->call(CarrerasSeeder::class);
         $this->call(MateriasSeeder::class);
+        $this->call(GrupoSeeder::class);
+        $this->call(CurriculaSeeder::class);
+
     }
 }
