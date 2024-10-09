9c3da1d20b983cf9714aefa2ed5da485431bc9df
Farid <geelgaga@gmail.com>
2024-10-08 21:14:31 -0400

    Creacion de la api

---
+++ gest-ahoex-backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     title="API de Gestión de Ambientes",
+ *     version="1.0.0",
+ *     description="Documentación de la API para el módulo de Ambiente en el proyecto universitario.",
+ *     @OA\Contact(
+ *         email="soporte@tudominio.com"
+ *     )
+ * )
+ */
