9b1c42e7811af9dc7275ce01b47075078b063387
William Calle Pinto <cwilliam14c@gmail.com>
2024-10-16 09:01:28 -0400

    Test actualizar tipo de uso

--- gest-ashoex-backend/tests/Feature/Ambiente/UsoControllerTest.php
+++ gest-ashoex-backend/tests/Feature/Ambiente/UsoControllerTest.php
@@ -51,5 +51,52 @@ class UsoControllerTest extends TestCase
                 'message' => 'Error',
             ]);
     }
+
+    public function testActualizarUsoDeAmbiente(): void
+    {
+        $id_uso = 1;
+        $uso = [
+            'tipo_uso' => 'Aula Comun',
+            'id_aula' => 1
+        ];
+
+        $response = $this->putJson("/api/usos/{$id_uso}", $uso);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'data' => [
+                    'id_uso' => 1,
+                    'tipo_uso' => $uso['tipo_uso'],
+                    'id_aula' => $uso['id_aula']
+                ],
+                'error' => null,
+                'message' => 'Tipo de uso actualizado exitosamente',
+            ]);
+    }
+
+    public function testActualizarUsoAmbienteConIdInvalido(): void
+    {
+        $id_uso = 999;
+        $uso = [
+            'tipo_uso' => '',
+            'id_aula' => 1
+        ];
+
+        $response = $this->putJson("/api/usos/{$id_uso}", $uso);
+
+        $response->assertStatus(404)
+            ->assertJson([
+                'success' => false,
+                'data' => [],
+                'error' => [
+                    [
+                        'status' => 404,
+                        'detail' => "No existe un Uso de ambiente con el ID: {$id_uso}"
+                    ]
+                ],
+                'message' => 'Error',
+            ]);
+    }
 }
 
