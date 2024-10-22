5ab45ff1a85547d47cc35f9cde278fa6f48c9d4c
JORGE ALEJANDRO GARCIA <102833071+Alegithub01@users.noreply.github.com>
2024-10-22 00:01:11 -0400

    Revert "Curricula gustavo"

--- gest-ashoex-frontend/src/services/listaCarreraService.js
+++
@@ -1,9 +0,0 @@
-const fetchListaCarrera = async () => {
-    const response = await fetch('http://localhost:8000/api/carreras');
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return await response.json();
-};
-
-export default fetchListaCarrera;
