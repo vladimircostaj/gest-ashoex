260e0983d40abc92dc703f9ea686c99c3342237a
danny0406 <kevinpoma312@gmail.com>
2024-10-23 18:54:21 -0400

    refactor : listar presonal academico

--- gest-ashoex-backend/routes/api.php
+++ gest-ashoex-backend/routes/api.php
@@ -2,20 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\{
-	HealthController,
-	PersonalAcademicoController,
-    ListaPersonalAcademicoController
-};
+use App\Http\Controllers\HealthController;
+use App\Http\Controllers\PersonalAcademicoController;
+
 
 Route::get('/health', [HealthController::class, 'check']);
 
 Route::controller(PersonalAcademicoController::class)->group(function () {
-	Route::get('/personal-academicos', 'index');
+    Route::get('/personal-academicos', 'ListaPersonalAcademico');
     Route::get('/personal-academicos/{id}',  'show');
     Route::post('/personal-academico', 'registrar');
     Route::put('/personal-academico/{id}','update');
 	Route::patch('/personal-academicos/dar-baja', 'darDeBaja');
 });
-
-Route::get('/ListaPersonalAcademico', [ListaPersonalAcademicoController::class, 'ListaPersonalAcademico']);
