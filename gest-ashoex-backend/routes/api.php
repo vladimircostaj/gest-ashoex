3c66c6f7a8be55a74e5ac740d4a233ea240d73e0
danny0406 <kevinpoma312@gmail.com>
2024-10-23 18:47:24 -0400

    delete: middleware

--- gest-ashoex-backend/routes/api.php
+++ gest-ashoex-backend/routes/api.php
@@ -8,20 +8,14 @@ use App\Http\Controllers\{
     ListaPersonalAcademicoController
 };
 
-use App\Http\Middleware\Sanitizer;
-
 Route::get('/health', [HealthController::class, 'check']);
 
 Route::controller(PersonalAcademicoController::class)->group(function () {
 	Route::get('/personal-academicos', 'index');
     Route::get('/personal-academicos/{id}',  'show');
-
     Route::post('/personal-academico', 'registrar');
-
     Route::put('/personal-academico/{id}','update');
-        
-	Route::patch('/personal-academicos/dar-baja', 'darDeBaja')
-        ->middleware(Sanitizer::class);
+	Route::patch('/personal-academicos/dar-baja', 'darDeBaja');
 });
 
 Route::get('/ListaPersonalAcademico', [ListaPersonalAcademicoController::class, 'ListaPersonalAcademico']);
