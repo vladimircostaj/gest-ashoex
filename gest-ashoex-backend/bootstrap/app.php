cb18f42891b89f8308f632bb1c4eff1207a05526
Jairo Andree Maida Rojas <jairotrabaja123@gmail.com>
2024-10-25 16:05:23 -0400

    fix: deleted strange text in parameter

--- gest-ashoex-backend/bootstrap/app.php
+++ gest-ashoex-backend/bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
         web: __DIR__ . '/../routes/web.php',
