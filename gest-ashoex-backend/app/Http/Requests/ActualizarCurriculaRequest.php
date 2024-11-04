4520c2d09257ac22bdf8ba8bf9e3ea805a0fd96c
Arieleandrovv <Arieleandrovv@outlook.com>
2024-11-04 13:53:00 -0400

    correccion de formato igual a guardar curricula

--- gest-ashoex-backend/app/Http/Requests/ActualizarCurriculaRequest.php
+++ gest-ashoex-backend/app/Http/Requests/ActualizarCurriculaRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use App\Models\Curricula;
+use App\Models\Carrera;
 
 
 class ActualizarCurriculaRequest extends FormRequest
@@ -23,45 +24,52 @@ class ActualizarCurriculaRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    {
-        return [
-            'carrera_id' => [
-                'required',
-                'integer',
-                'exists:carreras,id',
-            ],
-            'materia_id' => [
-                'required',
-                'integer',
-                'exists:materias,id',
-                function ($attribute, $value, $fail) {
-                    $curriculaId = $this->route('id');
-                    $carreraId=$this->input('carrera_id');
-                    if (!is_numeric($value)) {
-                        return;
-                    }else if(!is_numeric($carreraId)){
-                        return;
-                    }
-                    
-                    if (!$curriculaId) {
-                        return;
-                    }    
-                    $exists = Curricula::where('carrera_id', $this->input('carrera_id'))
+{
+    return [
+        'carrera_id' => [
+            'required',
+            'integer',
+            'exists:carreras,id',
+        ],
+        'materia_id' => [
+            'required',
+            'integer',
+            'exists:materias,id',
+            function ($attribute, $value, $fail) {
+                $curriculaId = $this->route('id');
+                $carreraId = $this->input('carrera_id');
+                if (!is_numeric($value) || !is_numeric($carreraId)) {
+                    return;
+                }
+
+                if ($curriculaId) {
+                    $exists = Curricula::where('carrera_id', $carreraId)
                         ->where('materia_id', $value)
                         ->where('id', '!=', $curriculaId)
                         ->exists();
-    
+
                     if ($exists) {
-                        $fail("La combinación de carrera y materia ya existe en la base de datos.");
+                        $fail("Ya existe una Curricula con los mismos carrera_id y materia_id");
                     }
-                },
-            ],
-            'nivel' => [
-                'required',
-                'integer',
-                'min:1',
-            ],
-            'electiva' => 'required|boolean',
-        ];
-    }
+                }
+            },
+        ],
+        'nivel' => [
+            'required',
+            'integer',
+            'min:1',
+            function ($attribute, $value, $fail) {
+                $carreraId = $this->input('carrera_id');
+                if (!is_numeric($value) || !is_numeric($carreraId)) {
+                    return;
+                }
+                $carrera = Carrera::find($carreraId);
+                if ($carrera && $value > $carrera->nro_semestres) {
+                    $fail("El nivel no puede ser mayor que el número de semestres de la carrera");
+                }
+            },
+        ],
+        'electiva' => 'required|boolean',
+    ];
+}
 }
