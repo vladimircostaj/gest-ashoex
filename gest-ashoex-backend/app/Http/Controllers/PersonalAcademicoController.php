b9bcfa461812d64393a913aac074270338420e31
Luis Fernando Cárdenas <201803077@est.umss.edu>
2024-11-13 11:34:43 -0400

    se actuliazo el formato del reques

--- gest-ashoex-backend/app/Http/Controllers/PersonalAcademicoController.php
+++ gest-ashoex-backend/app/Http/Controllers/PersonalAcademicoController.php
@@ -21,11 +21,11 @@ class PersonalAcademicoController extends Controller
                     'success' => false,
                     'data' => [],
                     'error' => [
-                        'code' => 204,
+                        'code' => 204, 
                         'message' => 'No se encontró datos'
                     ],
                     'message' => 'Lista vacía'
-                ], 204);
+                ], 200); 
             }
     
             
@@ -64,41 +64,28 @@ class PersonalAcademicoController extends Controller
     }
     
 
-    public function darDeBaja(int $id): JsonResponse
+    public function darDeBaja(Request $request): JsonResponse
     {
         try {
-            $personalAcademicoID = $id;
+            $personalAcademicoID = $request->id;
             $personalAcademico = PersonalAcademico::find($personalAcademicoID);
             if (!$personalAcademico) {
-                return parent::response(
-                    false,
-                    [], 
-                    'El personal academico seleccionado no existe.', 
-                    [
-                        'code' => 404,
-                        'message' => 'Personal no encontrado.'
-                    ]
-                );
+                return response()->json([
+                    'data' => 'El personal academico seleccionado no existe.'
+                ], 404);
             }
-            $dadoBaja = $personalAcademico->darBaja();
-            return parent::response(
-                $dadoBaja, 
-                $personalAcademico->toArray(), 
-                (
-                    $dadoBaja? 
+            return response()->json([
+                'data' => (
+                    $personalAcademico->darBaja() ?
                     'Se dio de baja correctamente al personal academico: ' . $personalAcademico->nombre :
                     'El personal academico: ' . $personalAcademico->nombre . ' ya fue dado de baja anteriormente, no puede dar de baja a un personal academico dado de baja.'
-                ),
-                ($dadoBaja? []: 
-                    ['code' => 400, 'message' => 'Dado de baja 2 veces.'])
-            );
+                )
+            ], 200);
         } catch (\Exception $e) {
-            return parent::response(
-                false, 
-                [],
-                'Ocurrio un error en el servidor.',
-                ['code' => 500, 'message' => $e->getMessage()]
-            );
+            return response()->json([
+                'message' => 'Server error',
+                'error' => $e->getMessage()
+            ], 500);
         }
     }
 
@@ -191,105 +178,22 @@ class PersonalAcademicoController extends Controller
 
         if (!$personalAcademico) {
             return response()->json([
-                'success' => false,
-                'data' => null,
-                'error' => [
-                    'code' => 404,
-                    'message' => 'Personal académico no encontrado.'
-                ],
-                'message' => 'Error en la solicitud.'
+                'message' => 'Personal académico no encontrado.'
             ], 404);
         }
 
-        $emailExistente = PersonalAcademico::where('email', $request->input('email'))->where('id', '!=', $id)->exists();
+        $personalAcademico->nombre = $request->input('nombre');
+        $personalAcademico->email = $request->input('email');
+        $personalAcademico->telefono = $request->input('telefono');
+        $personalAcademico->estado = $request->input('estado');
+        $personalAcademico->tipo_personal_id = $request->input('tipo_personal_id');
 
-        if ($emailExistente) {
-            return response()->json([
-                'success' => false,
-                'data' => null,
-                'error' => [
-                    'code' => 409,
-                    'message' => 'El correo electrónico ya está registrado.'
-                ],
-                'message' => 'Error en la solicitud.'
-            ], 409);
-        }
+        $personalAcademico->save();
 
-        try {
-
-            $request->validate([
-                'nombre' => 'required|string',
-                'email' => 'required|email',
-                'telefono' => 'required|string',
-                'estado' => 'required|string',
-                'tipo_personal_id' => 'required|integer|exists:tipo_personals,id'
-            ]);
-
-            $personalAcademico->nombre = $request->input('nombre');
-            $personalAcademico->email = $request->input('email');
-            $personalAcademico->telefono = $request->input('telefono');
-            $personalAcademico->estado = $request->input('estado');
-            $personalAcademico->tipo_personal_id = $request->input('tipo_personal_id');
-
-            $personalAcademico->save();
-
-            return response()->json([
-                'success' => true,
-                'data' => $personalAcademico,
-                'error' => null,
-                'message' => 'Operación exitosa.'
-            ], 200);
-
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'data' => null,
-                'error' => [
-                    'code' => 400,
-                    'message' => 'Datos de entrada inválidos.'
-                ],
-                'message' => 'Error en la solicitud.'
-            ], 400);
-        }
-
+        return response()->json([
+            'message' => 'Personal académico actualizado exitosamente.',
+            'personal_academico' => $personalAcademico
+        ], 200);
     }
-    public function test_obtener_lista_de_personal_academico_exitosamente()
-    {
-        // Se inserta datos en la tabla
-        DB::table('tipo_personals')->insert([
-            ['id' => 1, 'nombre' => 'Auxiliar'],
-            ['id' => 2, 'nombre' => 'Titular']
-        ]);
 
-        DB::table('personal_academicos')->insert([
-            [
-                'id' => 1,
-                'name' => 'Patrick Almanza',
-                'email' => 'patralm@gmail.com',
-                'telefono' => '69756409',
-                'estado' => 'Activo',
-                'tipo_personal_id' => 1
-            ]
-        ]);
-
-        $response = $this->get('/personal-academicos');
-
-        // Verificar que la respuesta sea correcta 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'success' => true,
-            'message' => 'Operación exitosa',
-            'data' => [
-                [
-                    'Tipo_personal' => 'Auxiliar',
-                    'telefono' => '69756409',
-                    'personal_academico_id' => 1,
-                    'tipo_personal_id' => 1,
-                    'name' => 'Patrick Almanza',
-                    'email' => 'patralm@gmail.com',
-                    'estado' => 'Activo'
-                ]
-            ]
-        ]);
-    }
 }
