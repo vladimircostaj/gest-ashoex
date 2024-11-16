0f8700c4dae511e881b77f98910403f9c710e81a
William Calle Pinto <cwilliam14c@gmail.com>
2024-11-16 17:25:13 -0400

    Eliminacion de carpetas y archivos en el frontend

--- gest-ashoex-frontend/src/services/healthService.js
+++
@@ -1,9 +0,0 @@
-const fetchHealthStatus = async () => {
-    const response = await fetch('http://localhost:8000/api/health');
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return await response.json();
-};
-
-export default fetchHealthStatus;
