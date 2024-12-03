730fe31f8e383aacb59a5406090b1c5c293a3bd9
Cristian <abcristianfloreslu@gmail.com>
2024-12-03 13:54:45 -0400

    Listado Edificios conexion BD

--- gest-ashoex-frontend/src/pages/Ambiente/listar_edificios_page.jsx
+++ gest-ashoex-frontend/src/pages/Ambiente/listar_edificios_page.jsx
@@ -1,23 +1,57 @@
+import { useState, useEffect } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import "./listar_ambientes.css";
 import Title from "../../components/typography/title";
 
 const ListaEdificios = () => {
+  // Lista de edificios base de datos
+  /*const [edificios, setEdificios] = useState([]); // Estado inicial como un arreglo
+  const [loading, setLoading] = useState(true); // Estado de cargando
+  const [error, setError] = useState(null); // Estado para errores
+
+  const fetchEdificios = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/edificios"); // Reemplaza con tu URL real
+      if (!response.ok) {
+        throw new Error("Error al obtener los datos");
+      }
+      const { data } = await response.json(); // Extrae la propiedad `data` directamente
+      setEdificios(data); // Asigna solo `data` al estado
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchEdificios();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando datos...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+ */
   // Lista de edificios estática
+  
   const edificios = [
     {
-      id: 1,
-      nombre: "Edificio Nuevo",
+      id_edificio: 1,
+      nombre_edificio: "Edificio Nuevo",
       geolocalizacion: "19.4326° N, 99.1332° W",
     },
     {
-      id: 2,
-      nombre: "Edificio Multiacademico",
+      id_edificio: 2,
+      nombre_edificio: "Edificio Multiacademico",
       geolocalizacion: "34.0522° N, 118.2437° W",
     },
     {
-      id: 3,
-      nombre: "Edificio Memi",
+      id_edificio: 3,
+      nombre_edificio: "Edificio Memi",
       geolocalizacion: "48.8566° N, 2.3522° E",
     },
   ];
@@ -42,9 +76,9 @@ const ListaEdificios = () => {
         </thead>
         <tbody>
           {edificios.map((edificio) => (
-            <tr key={edificio.id}>
-              <td>{edificio.id}</td>
-              <td>{edificio.nombre}</td>
+            <tr key={edificio.id_edificio}>
+              <td>{edificio.id_edificio}</td>
+              <td>{edificio.nombre_edificio}</td>
               <td>{edificio.geolocalizacion}</td>
               <td>
                 <a href="#" className="edit mr-6 ml-6">
