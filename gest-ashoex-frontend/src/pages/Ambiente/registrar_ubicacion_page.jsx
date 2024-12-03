af1dac360bb1d221330f8f52e0d85e91822d3965
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-12-03 10:50:04 -0400

    realizando validaciones del formulario

--- gest-ashoex-frontend/src/pages/Ambiente/registrar_ubicacion_page.jsx
+++ gest-ashoex-frontend/src/pages/Ambiente/registrar_ubicacion_page.jsx
@@ -12,6 +12,7 @@ const RegistrarUbicacionForm = () => {
     const [edificios, setEdificios] = useState([]);
     const [edificioSelected, setEdificioSelected] = useState("");
     const [numeroPiso, setNumeroPiso] = useState("");
+    const [errors, setErrors] = useState({});
     const api = "http://127.0.0.1:8000/api";
 
     const fetchEdificios = async () => {
@@ -42,12 +43,31 @@ const RegistrarUbicacionForm = () => {
         }
     };
 
+    const validate = () => {
+        const newErrors = {};
+        if (!numeroPiso) {
+            newErrors.numeroPiso = "El número de piso es requerido";
+        } else if (numeroPiso > 20) {
+            newErrors.numeroPiso = "El número de piso no puede ser mayor a 20";
+        }
+        if (!edificioSelected) {
+            newErrors.edificioSelected = "El edificio es requerido";
+        }
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
     const handleCancel = () => {
         console.log("Registro cancelado");
     };
 
-    const handleSave = () => {
-        console.log("Datos guardados:", { numeroPiso, edificioSelected });
+    const handleSave = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            console.log("Datos guardados:", { numeroPiso, edificioSelected });
+        } else {
+            console.log("Errores en el formulario:", errors);
+        }
     };
 
     return (
@@ -70,7 +90,9 @@ const RegistrarUbicacionForm = () => {
                             input: { width: "100%" },
                         }}
                     />
-
+                    
+                    {errors.numeroPiso && <div className="error">{errors.numeroPiso}</div>}
+                    
                     <SelectField
                         label="Edificio:"
                         id="id_ubicacion"
@@ -88,6 +110,8 @@ const RegistrarUbicacionForm = () => {
                             select: { width: "100%" },
                         }}
                     />
+
+                    {errors.edificioSelected && <div className="error">{errors.edificioSelected}</div>}     
 
                     <div className="d-flex justify-content-between gap-2 mt-3">
                         <CancelButton onClick={handleCancel} />
