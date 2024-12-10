e5e202b0607ea24d5726e286dd3cb6d903698924
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-12-10 19:34:17 -0400

    bugs fix

--- gest-ashoex-frontend/src/App.jsx
+++ gest-ashoex-frontend/src/App.jsx
@@ -7,7 +7,6 @@ import SlideBar from "./components/common/slidebar.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistrarAmbiente from "./pages/Ambiente/registrar_ambiente_page.jsx";
 import RegistrarUbicacionForm from './pages/Ambiente/registrar_ubicacion_page.jsx';
-import ListaAmbientes from "./pages/Ambiente/listar_ambientes_page.jsx";
 import RegistrarPersonal from "./pages/Personal/registrar_personal.jsx";
 import EditarAmbiente from "./pages/Ambiente/editar_ambiente_page.jsx";
 import RegistrarMateriaForm from "./pages/Curricula/registrar_materia_page.jsx";
@@ -33,7 +32,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HealthCheck />}>
           <Route path="/registrar-ambiente" element={<RegistrarAmbiente />} />
-          <Route path="/lista-ambientes" element={<ListaAmbientes />} />
           <Route path="/lista-usos" element={<  ListarUsos/>} />
           <Route path="/lista-edificios" element={<ListaEdificios/>} />
           <Route path="/registrar-personal" element={<RegistrarPersonal />} />
