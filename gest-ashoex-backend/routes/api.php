c474581c735fb068f1a47ac4c779636daa35f3db
Daye0210 <202202138@est.umss.edu>
2024-12-10 11:41:23 -0400

    Añadiendo url

--- gest-ashoex-backend/routes/api.php
+++ gest-ashoex-backend/routes/api.php
@@ -12,6 +12,9 @@ Route::controller(PersonalAcademicoController::class)->group(function () {
     Route::get('/personal-academicos', 'ListaPersonalAcademico');
     Route::get('/personal-academicos/{id}',  'show');
     Route::post('/personal-academico', 'registrar');
-    Route::put('/personal-academico/{id}','update');
+    Route::put('/personal-academicos/{id}','update');
 	Route::patch('/personal-academicos/{id}/dar-baja', 'darDeBaja');
+    Route::post('/registrar-tipos-personal',  'registrarTipoPersonal');
+    Route::get('/tipos-personal',  'getTiposPersonal');
+
 });
